f44ecd580010e0274cdf1c0e95c39fcddc7bb13e
Aliya Yakupova <34814028+rutaolta@users.noreply.github.com>
2021-10-29 23:36:58 +0300

    Update mini_quast.py

--- mini_quast.py
+++ mini_quast.py
@@ -2,7 +2,8 @@ from os import mkdir
 import pandas as pd
 import numpy as np
 
-def get_fasta_info(input='scripts/saccharomyces_cerevisiae.fasta', output='scripts/fasta_info.csv', window_size=10_000):
+
+def get_fasta_info(input, output, window_size=10_000):
     file = open(input).read()
 
     seq_iter = map(lambda seq: seq.split("\n", 1), file.split(">"))
@@ -27,14 +28,18 @@ def get_fasta_info(input='scripts/saccharomyces_cerevisiae.fasta', output='scrip
     df.to_csv(output, index=False)
     return df
 
+
 def get_genome_len(lenghts):
     return np.sum(lenghts)
+
 
 def get_max_scaffold_len(lenghts):
     return np.max(lenghts)
 
+
 def get_scaffold_cnt(df):
     return len(df)
+
 
 def N50_L50(lenghts, genome_size, statictic='N'):
     sorted_lenghts = sorted(lenghts, reverse=True)
@@ -46,17 +51,22 @@ def N50_L50(lenghts, genome_size, statictic='N'):
 
     return index + 1 if statictic == 'L' else sorted_lenghts[index]
 
+
 def get_N50(lenghts):
     return N50_L50(lenghts, np.sum(lenghts), 'N')
+
 
 def get_L50(lenghts):   
     return N50_L50(lenghts, np.sum(lenghts), 'L')
 
+
 def get_NG50(lenghts, genome_size):
     return N50_L50(lenghts, genome_size, 'N')
 
+
 def get_N_cnt(sequence):
     return sequence.count('N')   
+
 
 def GC(seq):
     gc = sum(seq.count(x) for x in ["G", "C", "g", "c", "S", "s"])
@@ -65,16 +75,20 @@ def GC(seq):
     except ZeroDivisionError:
         return 0.0
 
+
 def get_GC_content(seq, window=50_000):
     values = []
     for i in range(0, len(seq), window):
         values.append(GC(seq[i : i + window]))
     return values
 
-df = get_fasta_info()
-print("Genome length: ", get_genome_len(df['scaffold_length']))
-print("Max scaffold length: ", get_max_scaffold_len(df['scaffold_length']))
-print("Scaffold count: ", get_scaffold_cnt(df))
-print("N50: ", get_N50(df['scaffold_length']))
-print("L50: ", get_L50(df['scaffold_length']))
-print("NG50: ", get_NG50(df['scaffold_length'], genome_size=12_242_942))
+
+def test(input='scripts/saccharomyces_cerevisiae.fasta', output='scripts/fasta_info.csv'):
+    df = get_fasta_info(input, output)
+    print("Genome length: ", get_genome_len(df['scaffold_length']))
+    print("Max scaffold length: ", get_max_scaffold_len(df['scaffold_length']))
+    print("Scaffold count: ", get_scaffold_cnt(df))
+    print("N50: ", get_N50(df['scaffold_length']))
+    print("L50: ", get_L50(df['scaffold_length']))
+    print("NG50: ", get_NG50(df['scaffold_length'], genome_size=12_242_942))
+
